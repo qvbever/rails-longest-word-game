d55d337ab4f9924a9f65620328bb607ff24c8822
quentin vanbever <quentin.vanbever@gmail.com>
2024-09-18 22:26:40 +0200

    small issues

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,33 +3,30 @@ require 'json'
 
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }.join
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
-    @word = params[:word].upcase
-    @letters = params[:letters].upcase
-    if valid_word?(@word, @letters)
-      if english_word?(@word)
-        @answer = "Congratulation! #{@word} is a valid English word"
-      else
-        @answer = "Sorry but #{@word} does not seems an English word"
-      end
-    else
-      @answer = "Sorry but #{@word} can't be built out of #{@letters.join(', ')}"
-    end
+    @word = (params[:word] || "").upcase
+    @letters = params[:letters].split
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   private
 
-  def valid_word?(word, letters)
+  def included?(word, letters)
     word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
-    url = URI.parse("https://wagon-dictionary.herokuapp.com/#{word}")
-    response = JSON.parse(URI.open(url).read)
-    response['found']
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
